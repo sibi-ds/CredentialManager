019ec68673d80d276457193cf81237fba8205342
sibi <sibi.dhanapal@ideas2it.com>
2022-05-19 21:31:22 +0530

    unit testing

--- employee/employee_service_tests.py
+++ employee/employee_service_tests.py
@@ -1,38 +1,46 @@
+from unittest import mock
+from unittest.mock import MagicMock, patch
+
 from django.test import TestCase
-# from unittest import TestCase
-from unittest.mock import patch
 
 from employee.models import Employee
 from employee.service import employee_service
+from employee.serializers import EmployeeSerializer
 
 from organization.models import Organization
 
 
-class TestEmployeeService(TestCase):
+class EmployeeServiceTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         organization = Organization.objects.create(
             organization_id=1, name='ideas2it',
-            email='admin@ideas2it.com', password='admin'
+            email='admin@ideas2it.com', password='admin',
         )
 
-        Employee.objects.create(
+        employee = Employee.objects.create(
             employee_id=1, name='sibi',
             email='sibi@ideas2it.com', password='sibi',
-            created_by=organization,
-            organization=organization
+            organization=organization,
+            created_by=organization
         )
 
-    def test_create_employee(self):
+    # @mock.patch('employee.models.Employee.save',
+    #             return_value=Employee.objects.filter(employee_id=1).first())
+    @mock.patch('employee.models.Employee.objects.create',
+                return_value=Employee.objects.filter(employee_id=1))
+    def test_create_employees(self, mocked_create_employee,
+                              ):
+        print(mocked_create_employee.return_value)
+
         payload = {
-            'employee_id': 7,
-            'name': 's',
-            'email': 's@ideas2it.com',
-            'password': 's'
+            'employee_id': 1,
+            'name': 'sibi1',
+            'email': 'sibi1@ideas2it.com',
+            'password': 'sibi1',
         }
 
-        response = employee_service.create_employee(1, payload)
-
+        # response = employee_service.create_employee(1, payload)
+        response = Employee.objects.create(dict())
         print(response)
-
